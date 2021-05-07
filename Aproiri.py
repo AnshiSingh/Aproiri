abb525231353c2b9e32e0464190c6c808b4767cf
Anshika Singh <58014942+AnshiSingh@users.noreply.github.com>
2021-05-07 21:27:54 +0530

    Update Aproiri.py

--- Aproiri.py
+++ Aproiri.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-get_ipython().system('pip install apyori')
-
-
-# In[11]:
-
+# Aproiri
+get_ipython().system('pip install apyori') 
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,9 +7,6 @@ import pandas as pd
 
 
 # ### DATA PREPROCESSING
-
-# In[14]:
-
 
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
 transactions = []
@@ -29,9 +17,6 @@ print(transactions)
 
 # ### TRAINING THE APRIORI MODEL ON DATASET
 
-# In[20]:
-
-
 from apyori import apriori
 rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
 
@@ -40,22 +25,11 @@ rules = apriori(transactions = transactions, min_support = 0.003, min_confidence
 
 # ### Displaying the first results coming directly from the output of the apriori function
 
-# In[21]:
-
-
 results = list(rules)
-
-
-# In[23]:
-
-
 results
 
 
 # ### Putting the results well organised into a Pandas DataFrame
-
-# In[25]:
-
 
 def inspect(results):
     lhs         = [tuple(result[2][0][0])[0] for result in results]
@@ -69,16 +43,10 @@ resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side',
 
 # ### DISPLAYING THE RESULTS NON SORTED
 
-# In[28]:
-
-
 resultsinDataFrame
 
 
 # ### DISPLAYING THE RESULT SORTED BY DESCENDING LIFT
-
-# In[29]:
-
 
 resultsinDataFrame.nlargest(n = 10, columns = 'Lift')
 
